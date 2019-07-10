e23b63414c65aa8ea734081b935b15aafe58ba7b
Kirill <thegaldren@gmail.com>
2019-07-10 11:44:11 +0300

    add «lastInteractions» to Person

--- src/com/trulden/Person.java
+++ src/com/trulden/Person.java
@@ -1,12 +1,16 @@
 package com.trulden;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Person implements Serializable {
+
     private String name;
+    private HashMap<String, Interaction> lastInteractions;
 
     Person(String name){
         this.name = name;
+        lastInteractions = new HashMap<>();
     }
 
     public void setName(String name){
@@ -15,5 +19,9 @@ public class Person implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+    public void addInteraction(Interaction interaction){
+        lastInteractions.put(interaction.getType(), interaction);
     }
 }
