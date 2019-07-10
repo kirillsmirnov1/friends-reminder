09ce6dbbe4333c94d85393b620bbae3c9e71545b
Kirill <thegaldren@gmail.com>
2019-07-10 10:36:48 +0300

    Util class with serialization methods

---
+++ src/com/trulden/Util.java
@@ -0,0 +1,39 @@
+package com.trulden;
+
+import java.io.*;
+
+public class Util {
+
+    // Reading object from file
+    public static void serialize(Object object, String fileName){
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                new FileOutputStream(fileName))){
+
+            objectOutputStream.writeObject(object);
+
+        } catch (Exception e){
+            System.out.println("Serialization error");
+            e.printStackTrace();
+        }
+    }
+
+    // Writing object to file
+    public static Object deserialize(String fileName) {
+
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(
+                new FileInputStream(fileName))){
+
+            return objectInputStream.readObject();
+
+        } catch (Exception e){
+            if(e.getClass() == FileNotFoundException.class) {
+                System.out.println("File «" + fileName + "» not found");
+            } else {
+                System.out.println("Error in deserialization");
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}
