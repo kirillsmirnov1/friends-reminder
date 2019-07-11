4adab162687fc168cf5c5cc001255b05f603330e
Kirill <thegaldren@gmail.com>
2019-07-11 09:22:38 +0300

    beanify Person

--- src/com/trulden/Person.java
+++ src/com/trulden/Person.java
@@ -9,6 +9,39 @@ public class Person implements Serializable {
     private String name;
     private HashMap<String, Interaction> lastInteractions;
 
+    Person(){
+        name = "";
+        lastInteractions = new HashMap<>();
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Person that = (Person)obj;
+
+        if(!name.equals(that.getName()))
+            return false;
+
+        for(Map.Entry<String, Interaction> entry : lastInteractions.entrySet()){
+            if(!that.getLastInteractions().containsKey(entry.getKey())
+            || !that.getLastInteractions().get(entry.getKey()).getDate().equals(entry.getValue().getDate()))
+                return false;
+        }
+
+        return true;
+    }
+
     Person(String name){
         this.name = name;
         lastInteractions = new HashMap<>();
@@ -25,6 +58,10 @@ public class Person implements Serializable {
         return str.toString();
     }
 
+    public void addInteraction(Interaction interaction){
+        lastInteractions.put(interaction.getType(), interaction);
+    }
+
     public void setName(String name){
         this.name = name;
     }
@@ -33,7 +70,11 @@ public class Person implements Serializable {
         return name;
     }
 
-    public void addInteraction(Interaction interaction){
-        lastInteractions.put(interaction.getType(), interaction);
+    public HashMap<String, Interaction> getLastInteractions() {
+        return lastInteractions;
+    }
+
+    public void setLastInteractions(HashMap<String, Interaction> lastInteractions) {
+        this.lastInteractions = lastInteractions;
     }
 }
